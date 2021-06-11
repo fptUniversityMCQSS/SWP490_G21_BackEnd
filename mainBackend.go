c4d4b4cc276e26e173ee3753ee06ada66d01d776
haokxhe130473 <66771448+haokxhe130473@users.noreply.github.com>
2021-06-11 21:27:46 +0700

    abc
    Commit from khailq to haokx

---
+++ mainBackend.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"lib/controller"
+	"lib/model"
+	"net/http"
+)
+
+func main() {
+	//start echo
+	e := echo.New()
+	//website
+	e.GET("/", controller.Home)
+	e.GET("/home", controller.Home)
+	e.GET("/qa", controller.Qa)
+	e.GET("/knowledge", controller.Knowledge)
+	e.GET("/knowledge/upload", controller.KnowledgeUpload)
+	e.GET("/history", controller.History)
+	e.GET("/api", controller.ApiWeb)
+
+	e.GET("/test", func(context echo.Context) error {
+		return context.JSON(http.StatusOK, []model.Question{
+			//{
+			//	Content: "abc1",
+			//},
+			//{
+			//	Content: "abc2",
+			//},
+			//{
+			//	Content: "abc3",
+			//},
+		})
+	})
+	//api
+	e.Logger.Fatal(e.Start(":1323"))
+	//mot con vit xoe ra hai cai canh
+}
