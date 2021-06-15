a7b141ef63d6216c56ce0eb0f2070889adf78029
binhtbhe130864 <binhtbhe130864@fpt.edu.vn>
2021-06-15 23:08:06 +0700

    axios api login

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,12 +4,36 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+
+import './assets/css/bootstrap.css'
+import './assets/vendors/linericon/style.css'
+import './assets/css/font-awesome.min.css'
+import './assets/vendors/owl-carousel/owl.carousel.min.css'
+import './assets/vendors/lightbox/simpleLightbox.css'
+import './assets/vendors/nice-select/css/nice-select.css'
+import './assets/vendors/animate-css/animate.css'
+import './assets/vendors/jquery-ui/jquery-ui.css'
+import './assets/css/style.css'
+import './assets/css/responsive.css'
+//
+import './assets/js/jquery-3.2.1.min.js'
+import './assets/js/popper.js'
+import './assets/js/stellar.js'
+import './assets/vendors/nice-select/js/jquery.nice-select.min.js'
+import './assets/vendors/owl-carousel/owl.carousel.min.js'
+import './assets/js/jquery.ajaxchimp.min.js'
+import './assets/js/mail-script.js'
+import './assets/vendors/jquery-ui/jquery-ui.js'
+import './assets/vendors/counter-up/jquery.waypoints.min.js'
+import './assets/vendors/counter-up/jquery.counterup.js'
+
 Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
--- mainBackend.go
+++ mainBackend.go
@@ -5,6 +5,7 @@ import (
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"lib/controller"
 	"lib/model"
 	"lib/ultity"
@@ -26,6 +27,12 @@ func init() {
 func main() {
 	//start echo
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
+
 	//website
 	e.GET("/", controller.Home)
 	e.GET("/home", controller.Home)
