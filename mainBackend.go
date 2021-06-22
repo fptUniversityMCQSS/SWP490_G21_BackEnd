144642065717e75c5e9e47cca9017fec53ecf50a
haokxhe130473 <66771448+haokxhe130473@users.noreply.github.com>
2021-06-23 00:26:05 +0700

    ReCommit Knowledge Commit API by HaoKX

--- mainBackend.go
+++ mainBackend.go
@@ -13,12 +13,8 @@ import (
 )
 
 func init() {
-	dbConfig := ultity.ReadDBConfig()
-	stringConfig := dbConfig.DbUser + ":" +
-		dbConfig.DbPassword + "@tcp(" +
-		dbConfig.DbServer + ":" +
-		dbConfig.DbPort + ")/" +
-		dbConfig.Database + "?charset=utf8"
+	svConfig := ultity.ReadDBConfig()
+	stringConfig := svConfig.DbUser + ":" + svConfig.DbPassword + "@/" + svConfig.Database + "?charset=utf8"
 	orm.RegisterModel(new(model.Knowledge), new(model.Option), new(model.Question), new(model.User))
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 
@@ -28,29 +24,27 @@ func init() {
 	}
 }
 func main() {
-	svConfig := ultity.ReadServerConfig()
 	//start echo
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{"*"},
-		AllowMethods: []string{"GET", "POST", "PUT"},
 	}))
 	//website
 	e.GET("/", controller.Home)
 	e.GET("/home", controller.Home)
 	e.GET("/qa", controller.Qa)
-	e.GET("/knowledge", controller.Knowledge)
-	e.PUT("/knowledge", controller.KnowledgeUpload)
+	e.GET("/knowledge", controller.ListKnowledge)
+	e.PUT("/upload", controller.KnowledgeUpload)
 	e.GET("/history", controller.History)
 	e.GET("/api", controller.ApiWeb)
 	e.POST("/login", controller.LoginResponse)
-	e.PUT("/qa", controller.QaResponse, middleware.JWT([]byte("justAdmin")))
+	e.POST("/qa", controller.QaResponse, middleware.JWT([]byte("justAdmin")))
 
 	e.GET("/test", func(context echo.Context) error {
 		return context.JSON(http.StatusOK, []model.Question{})
 	})
 	//api
 
-	e.Logger.Fatal(e.Start(":" + svConfig.PortBackend))
+	e.Logger.Fatal(e.Start(":1323"))
 }
