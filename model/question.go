10242107ac44f2c9d9f9706197d0c8f128a9314d
khailq99 <khailqhe130839@fpt.edu.vn>
2021-06-23 18:30:53 +0700

    new model ExamTest fix model Question session testing

--- model/question.go
+++ model/question.go
@@ -1,8 +1,9 @@
 package model
 
 type Question struct {
-	Id      int64     `orm:"pk;auto"`
-	Content string    `json:"question_content" form:"question_content"`
-	Options []*Option `orm:"reverse(many)"`
-	Answer  *Option   `orm:"null;rel(one);on_delete(set_null)" json:"answer" form:"answer"`
+	Id       int64     `orm:"pk;auto"`
+	Content  string    `json:"question_content" form:"question_content"`
+	Options  []*Option `orm:"reverse(many)"`
+	Answer   *Option   `orm:"null;rel(one);on_delete(set_null)" json:"answer" form:"answer"`
+	ExamTest *ExamTest `orm:"rel(fk)"`
 }
