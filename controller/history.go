0e6fc51ae6eac1ab5a5aadf040fe656d3b6b4fce
thienlhhe130847 <56884065+thienlhhe130847@users.noreply.github.com>
2021-06-27 12:25:19 +0700

    fix database + api getExamById

--- controller/history.go
+++ controller/history.go
@@ -1,7 +1,41 @@
 package controller
 
-import "github.com/labstack/echo/v4"
+import (
+	"SWP490_G21_Backend/model"
+	"github.com/astaxie/beego/orm"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"strconv"
+	"strings"
+)
 
 func History(c echo.Context) error {
 	return c.File("views/history.html")
+}
+func GetExamById(c echo.Context) error {
+	token := strings.Split(c.Request().Header.Get("Authorization"), " ")[1]
+	values, _ := jwt.Parse(token, nil)
+	claims := values.Claims.(jwt.MapClaims)
+	userId := claims["userId"].(float64)
+	IntUserId := int64(userId)
+	id, _ := strconv.ParseInt(c.QueryParam("id"), 10, 64)
+	o := orm.NewOrm()
+	var examTest model.ExamTest
+	var questionAll []*model.Question
+	var answerAll []*model.Option
+	o.QueryTable("exam_test").Filter("id", id).All(&examTest)
+	if examTest.User.Id == IntUserId {
+		o.QueryTable("question").Filter("exam_test_id", id).RelatedSel().All(&questionAll)
+		examTest.Questions = questionAll
+		for _, question := range questionAll {
+			o.QueryTable("option").Filter("question_id_id", question.Id).All(&answerAll)
+			question.Options = answerAll
+		}
+		return c.JSON(http.StatusOK, examTest)
+	} else {
+		return c.JSON(http.StatusUnauthorized, "You dont have permission to access this")
+	}
+	return nil
+
 }
--- model/question.go
+++ model/question.go
@@ -1,7 +1,9 @@
 package model
 
 type Question struct {
-	Id       int64     `orm:"pk;auto"`
+	Id       int64 `orm:"pk;auto"`
+	Number   int64
+	Mark     float64
 	Content  string    `json:"question_content" form:"question_content"`
 	Options  []*Option `orm:"reverse(many)"`
 	Answer   *Option   `orm:"null;rel(one);on_delete(set_null)" json:"answer" form:"answer"`
