c7b33354aac981791a0ad5f4be4276feaf9cd15c
minhpahe130332 <84463418+minhpahe130332@users.noreply.github.com>
2021-07-07 21:49:58 +0700

    Thêm Name trong history api

--- model/response/history_Response.go
+++ model/response/history_Response.go
@@ -5,4 +5,5 @@ import "time"
 type HistoryResponse struct {
 	Id   int64     `orm:"pk;auto" json:"id" form:"id"`
 	Date time.Time `orm:"auto_now_add" json:"historyDate" form:"historyDate"`
+	Name string    `json:"historyName" form:"historyName"`
 }
