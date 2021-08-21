19fd65f9abad62c2a79a87fa9aa098c62b0e7496
thienlhhe130847 <56884065+thienlhhe130847@users.noreply.github.com>
2021-08-21 20:59:23 +0700

    fix small issues UserName

--- model/entity/user.go
+++ model/entity/user.go
@@ -1,12 +1,11 @@
 package entity
 
 type User struct {
-	Id        int64        `orm:"pk;auto" json:"id" form:"id"`
-	Username  string       `json:"username" form:"username"`
-	Password  string       `json:"password" form:"password"`
-	Email     string       `json:"email" form:"email"`
-	Phone     string       `json:"phone" form:"phone"`
-	Role      string       `json:"role" form:"role"`
-	FullName  string       `json:"fullName" form:"fullName"`
-	Knowledge []*Knowledge `orm:"reverse(many)" json:"list_knowledge" form:"list_knowledge"`
+	Id       int64  `orm:"pk;auto" json:"id" form:"id"`
+	Username string `json:"username" form:"username"`
+	Password string `json:"password" form:"password"`
+	Email    string `json:"email" form:"email"`
+	Phone    string `json:"phone" form:"phone"`
+	Role     string `json:"role" form:"role"`
+	FullName string `json:"fullName" form:"fullName"`
 }
